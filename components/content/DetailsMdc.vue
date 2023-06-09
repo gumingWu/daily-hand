fd06dfb710e3235e6e113a49ff74c7a09928de98
gumingWu <15626185054@163.com>
2023-06-09 09:59:50 +0800

    feat: detailsMdc

---
+++ components/content/DetailsMdc.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <details>
+    <summary>
+      <slot name="summary" />
+    </summary>
+    <slot />
+  </details>
+
+  <details>
+    <summary>这是哈哈哈</summary>
+    sahidhasd
+  </details>
+</template>
+
+<style scoped></style>
